f5ac95775c536ba845ccb8c115a4fb71fc865a1b
franc <franc@francisco>
2023-11-11 19:23:19 +0100

    Ejercicio1.-Escribe un programa en Java que muestre los números del 10 al 1, en ese orden. Debes utilizar una estructura de tipo while ó do-while.

---
+++ src/ejercicios/Ejercicio1.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Ejercicio1 {
+
+	public static void main (String [] args) {
+		int numero=10;
+		while(numero>0) {
+			System.out.println(numero);
+			numero--;				
+		}
+	}
+}
